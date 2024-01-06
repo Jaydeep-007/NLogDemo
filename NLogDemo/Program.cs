1d110dbfd76fa59459e63eddb60654684082e1a2
Jaydeep-07 <jaydeepvpatil225@gmail.com>
2024-01-06 12:11:40 +0530

    Add project files.

---
+++ NLogDemo/Program.cs
@@ -0,0 +1,35 @@
+using NLog.Extensions.Logging;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configure NLog
+builder.Services.AddLogging(logging =>
+{
+    logging.ClearProviders();
+    logging.SetMinimumLevel(LogLevel.Trace);
+});
+
+// Add NLog as the logger provider
+builder.Services.AddSingleton<ILoggerProvider, NLogLoggerProvider>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
